24e22ba78f2844badb8a71158d6b2ed52d6de1ba
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-08-30 12:55:07 +0530

    Order model added

---
+++ HotCoffee/Models/Order.swift
@@ -0,0 +1,28 @@
+//
+//  Order.swift
+//  HotCoffee
+//
+//  Created by Nalinda Wickramarathna on 2022-08-30.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappucino
+    case late
+    case espresso
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+}

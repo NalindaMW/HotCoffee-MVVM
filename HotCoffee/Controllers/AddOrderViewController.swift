00f681723fea82f57411c710a74af62fc6197f10
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-08-30 11:49:42 +0530

    Basic UI flow added

---
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddOrderViewController.swift
+//  HotCoffee
+//
+//  Created by Nalinda Wickramarathna on 2022-08-30.
+//
+
+import UIKit
+
+class AddOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

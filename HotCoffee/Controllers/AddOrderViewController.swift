f23c85ee11dc0075a24be57c2a15074b5abebdb4
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-08-31 22:19:12 +0530

    Coffee types table view populated

--- HotCoffee/Controllers/AddOrderViewController.swift
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class AddOrderViewController: UIViewController {
 
+    private var viewModel = AddCoffeeOrderViewModel()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension AddOrderViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.types.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoffeeTypeCell", for: indexPath)
+        
+        cell.textLabel?.text = viewModel.types[indexPath.row]
+        return cell
     }
-    */
-
+    
+    
 }
